5a9cae717de464cc6d01fbc29a051c2b4bf49e79
alessanderleite <alessander.logistica@gmail.com>
2019-08-23 20:10:19 -0300

    create the method salvar()

--- src/main/java/br/com/alessanderleite/boot/web/controller/DepartamentoController.java
+++ src/main/java/br/com/alessanderleite/boot/web/controller/DepartamentoController.java
@@ -1,12 +1,20 @@
 package br.com.alessanderleite.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alessanderleite.boot.domain.Departamento;
+import br.com.alessanderleite.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+	
+	@Autowired
+	private DepartamentoService service;
 
 	@GetMapping("/cadastrar")
 	public String cadastrar() {
@@ -16,5 +24,11 @@ public class DepartamentoController {
 	@GetMapping("/listar")
 	public String listar() {
 		return "/departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		return "redirect:/departamentos/cadastrar";
 	}
 }
