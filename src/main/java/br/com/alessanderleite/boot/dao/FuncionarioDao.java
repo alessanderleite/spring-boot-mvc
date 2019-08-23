47b7abb206a5ed1f5fd980a578c46e493fb22bb9
alessanderleite <alessander.logistica@gmail.com>
2019-08-23 11:29:02 -0300

    create the interface FuncionarioDao.java

---
+++ src/main/java/br/com/alessanderleite/boot/dao/FuncionarioDao.java
@@ -0,0 +1,14 @@
+package br.com.alessanderleite.boot.dao;
+
+import java.util.List;
+
+import br.com.alessanderleite.boot.domain.Funcionario;
+
+public interface FuncionarioDao {
+	
+	void save(Funcionario funcionario);
+	void update(Funcionario funcionario);
+	void delete(Long id);
+	Funcionario findById(Long id);
+	List<Funcionario> findAll();
+}
