aa0f41c21f7a2390ca570a940688b14226cf6bf1
alessanderleite <alessander.logistica@gmail.com>
2019-08-29 22:39:04 -0300

    create que method buscarPorCargo() inside FuncionarioService.java

--- src/main/java/br/com/alessanderleite/boot/service/FuncionarioService.java
+++ src/main/java/br/com/alessanderleite/boot/service/FuncionarioService.java
@@ -12,4 +12,5 @@ public interface FuncionarioService {
 	Funcionario buscarPorId(Long id);
 	List<Funcionario> buscarTodos();
 	List<Funcionario> buscarPorNome(String nome);
+	List<Funcionario> buscarPorCargo(Long id);
 }
