32211841beef4160084479ea366a3bcfbcbe5645
alessanderleite <alessander.logistica@gmail.com>
2019-08-29 22:15:06 -0300

    add query to make search by name inside the method findByNome()

--- src/main/java/br/com/alessanderleite/boot/dao/FuncionarioDaoImpl.java
+++ src/main/java/br/com/alessanderleite/boot/dao/FuncionarioDaoImpl.java
@@ -1,10 +1,17 @@
 package br.com.alessanderleite.boot.dao;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.alessanderleite.boot.domain.Funcionario;
 
 @Repository
 public class FuncionarioDaoImpl extends AbstractDao<Funcionario, Long> implements FuncionarioDao {
+
+	public List<Funcionario> findByNome(String nome) {
+		
+		return createQuery("select f from Funcionario f where f.nome like concat('%',1?, '%') ", nome);
+	}
 
 }
