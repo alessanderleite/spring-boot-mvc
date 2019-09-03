2cd13237db694fe88513a4cee60287efa90d5c50
alessanderleite <alessander.logistica@gmail.com>
2019-09-03 17:17:47 -0300

    add the @Component annotation

--- src/main/java/br/com/alessanderleite/boot/web/conversor/StringToInteger.java
+++ src/main/java/br/com/alessanderleite/boot/web/conversor/StringToInteger.java
@@ -1,7 +1,9 @@
 package br.com.alessanderleite.boot.web.conversor;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StringToInteger implements Converter<String, Integer>{
 
 	@Override
