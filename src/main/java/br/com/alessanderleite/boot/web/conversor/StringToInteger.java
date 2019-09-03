c8b95668a214c6e7cd6e4e33eb210bacdb383fa4
alessanderleite <alessander.logistica@gmail.com>
2019-09-03 17:15:04 -0300

    create the class named StringToInteger.java

---
+++ src/main/java/br/com/alessanderleite/boot/web/conversor/StringToInteger.java
@@ -0,0 +1,17 @@
+package br.com.alessanderleite.boot.web.conversor;
+
+import org.springframework.core.convert.converter.Converter;
+
+public class StringToInteger implements Converter<String, Integer>{
+
+	@Override
+	public Integer convert(String text) {
+		
+		text = text.trim();
+		
+		if (text.matches("[0-9]+")) {
+			return Integer.valueOf(text);
+		}
+		return null;
+	}
+}
